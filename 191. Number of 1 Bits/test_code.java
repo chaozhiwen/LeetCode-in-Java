e0446db501f6f6fe9e170a386396450553507984
chaozhiwen <42872823+chaozhiwen@users.noreply.github.com>
2018-10-25 13:25:03 +0800

    Create test_code.java

---
+++ 191. Number of 1 Bits/test_code.java
@@ -0,0 +1,29 @@
+class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int i=0;
+        
+        /*I ignored a question that the most bits of Integer is 32
+        while(n!=0){
+            if(n%2==1)
+                i++;
+            n/=2;
+        }*/
+        
+        String s=Integer.toBinaryString(n);
+        for(int j=0;j<s.length();j++){
+            if(s.charAt(j)=='1')
+                i++;
+        }
+        return i;
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        Solution s=new Solution();
+        System.out.println("0:"+s.hammingWeight(0));
+        System.out.println("1024:"+s.hammingWeight(1024));
+        System.out.println("10000000000000000000:"+s.hammingWeight(10000000000000000000));
+    }
+}
