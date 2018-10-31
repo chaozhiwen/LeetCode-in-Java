94aa7054a5289a841cc6cb5113cbc662c4da31c0
chaozhiwen <42872823+chaozhiwen@users.noreply.github.com>
2018-10-31 21:36:23 +0800

    Create code.java

---
+++ 476. Number Complement/code.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findComplement(int num) {
+        String a=Integer.toBinaryString(num);
+        String b="";
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)=='0'){
+                b+="1";
+            }else{
+                b+="0";
+            }
+        }
+        return Integer.valueOf(Integer.parseInt(b,2));
+    }
+}
