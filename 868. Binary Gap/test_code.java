503afec65ace2bf7e7221d5cc78c599f87c614f8
chaozhiwen <42872823+chaozhiwen@users.noreply.github.com>
2018-11-06 23:00:56 +0800

    Create test_code.java

---
+++ 868. Binary Gap/test_code.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int binaryGap(int N) {
+        int ans=0;
+        String s=Integer.toBinaryString(N);
+        Vector<Integer> v=new Vector<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1'){
+                v.add(i);
+            }
+        }
+        if(v.size()<2){
+            return 0;
+        }else{
+            int temp=0;
+            for(int i=0;i<v.size()-1;i++){
+                temp=v.get(i+1)-v.get(i);
+                if(temp>ans){
+                    ans=temp;
+                }
+            }
+        }
+        return ans;
+    }
+}
+class Main{
+	public static void main(String[] args){
+		Solution s=new Solution();
+		for(int i=0;i<=100000;i++){
+			System.out.println(i+"->"+s.binaryGap(i));
+		}
+	}
+}
