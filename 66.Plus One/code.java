f50483d0d7dbb66624e55400e1cbc0bbb96abe63
chaozhiwen <42872823+chaozhiwen@users.noreply.github.com>
2018-11-09 23:11:30 +0800

    Create code.java

---
+++ 66.Plus One/code.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits){
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]+=1;
+                break;
+            }
+            else{
+                digits[i]=0;
+            }
+        }
+        if(digits[0]==0){
+            int[] ans=new int[digits.length+1];
+            ans[0]=1;
+            for(int i=1;i<digits.length;i++){
+                ans[i]=digits[i];
+            }
+            return ans;
+        }
+        return digits;
+    }
+}
