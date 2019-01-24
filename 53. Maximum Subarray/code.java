ee64d481936729998bc306c4917f3e50cd1e4009
chaozhiwen <935115299@qq.com>
2019-01-24 17:12:52 +0800

    Update code.java

--- 53. Maximum Subarray/code.java
+++ 53. Maximum Subarray/code.java
@@ -1,39 +1,20 @@
-import java.util.*;
-
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        if(triangle.size()==1) return triangle.get(0).get(0);
-        
-        for(int i=1;i<triangle.size();i++)
-            for(int j=0;j<triangle.get(i).size();j++){
-                if(j==0)
-                    triangle.get(i).set(j,triangle.get(i-1).get(0)+triangle.get(i).get(0));
-                else if(j==triangle.get(i).size()-1)
-                    triangle.get(i).set(j,triangle.get(i-1).get(j-1)+triangle.get(i).get(j));
-                else
-                    triangle.get(i).set(j,triangle.get(i).get(j)+Math.min(triangle.get(i-1).get(j),triangle.get(i-1).get(j-1)));
-            }       
-        List<Integer> last=triangle.get(triangle.size()-1);
-        int ans=last.get(0);
-        for(int i=0;i<last.size();i++)
-            ans=Math.min(ans,last.get(i));
+    public int maxSubArray(int[] nums) {        
+        int ans=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]>0)
+                nums[i]+=nums[i-1];
+            ans=Math.max(ans,nums[i]);
+        }
         return ans;
     }
 }
 class Main{
 	public static void main(String[] args){
 		Solution s=new Solution();
-		List<Integer> l1=new ArrayList<>();
-		l1.add(1);
-		List<Integer> l2=new ArrayList<>();
-		l2.add(2);l2.add(3);
-		List<Integer> l3=new ArrayList<>();
-		l3.add(1);l3.add(7);l3.add(6);
-		List<Integer> l4=new ArrayList<>();
-		l4.add(35);l4.add(5);l4.add(3);l4.add(8);
-		List<List<Integer>> l=new ArrayList<>();
-		l.add(l1);l.add(l2);l.add(l3);l.add(l4);
-		
-		System.out.println(s.minimumTotal(l));
+		System.out.println(s.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+		System.out.println(s.maxSubArray(new int[]{1,-1,2,-2,3,-3}));
+		System.out.println(s.maxSubArray(new int[]{1,-1,-2,-3,-4,-5,-6,-7,99}));
+		System.out.println(s.maxSubArray(new int[]{1,2,3,4}));
 	}
 }
